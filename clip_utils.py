d1c0818e92cd72b00acac1c62adf0038db7255d5
Polaris <59070815+JKK-dr@users.noreply.github.com>
2024-07-27 17:01:57 +0800

    Update clip_utils.py

--- clip_utils.py
+++ clip_utils.py
@@ -1,5 +1,268 @@
 # clip相关的API
 
+import os
+from argparse import ArgumentParser
+import numpy as np
+import cv2
+from PIL import Image
+import torch
+import clip
+
+multiple_templates = [
+    "There is {} in the scene.",
+    "There is the {} in the scene.",
+    "a photo of {} in the scene.",
+    "a photo of the {} in the scene.",
+    "a photo of one {} in the scene.",
+    "I took a picture of of {}.",
+    "I took a picture of of my {}.",  # itap: I took a picture of
+    "I took a picture of of the {}.",
+    "a photo of {}.",
+    "a photo of my {}.",
+    "a photo of the {}.",
+    "a photo of one {}.",
+    "a photo of many {}.",
+    "a good photo of {}.",
+    "a good photo of the {}.",
+    "a bad photo of {}.",
+    "a bad photo of the {}.",
+    "a photo of a nice {}.",
+    "a photo of the nice {}.",
+    "a photo of a cool {}.",
+    "a photo of the cool {}.",
+    "a photo of a weird {}.",
+    "a photo of the weird {}.",
+    "a photo of a small {}.",
+    "a photo of the small {}.",
+    "a photo of a large {}.",
+    "a photo of the large {}.",
+    "a photo of a clean {}.",
+    "a photo of the clean {}.",
+    "a photo of a dirty {}.",
+    "a photo of the dirty {}.",
+    "a bright photo of {}.",
+    "a bright photo of the {}.",
+    "a dark photo of {}.",
+    "a dark photo of the {}.",
+    "a photo of a hard to see {}.",
+    "a photo of the hard to see {}.",
+    "a low resolution photo of {}.",
+    "a low resolution photo of the {}.",
+    "a cropped photo of {}.",
+    "a cropped photo of the {}.",
+    "a close-up photo of {}.",
+    "a close-up photo of the {}.",
+    "a jpeg corrupted photo of {}.",
+    "a jpeg corrupted photo of the {}.",
+    "a blurry photo of {}.",
+    "a blurry photo of the {}.",
+    "a pixelated photo of {}.",
+    "a pixelated photo of the {}.",
+    "a black and white photo of the {}.",
+    "a black and white photo of {}.",
+    "a plastic {}.",
+    "the plastic {}.",
+    "a toy {}.",
+    "the toy {}.",
+    "a plushie {}.",
+    "the plushie {}.",
+    "a cartoon {}.",
+    "the cartoon {}.",
+    "an embroidered {}.",
+    "the embroidered {}.",
+    "a painting of the {}.",
+    "a painting of a {}.",
+]
+
+def _init_lseg(self):
+        crop_size = 480  # 480
+        base_size = 520  # 520
+        if torch.cuda.is_available():
+            self.device = "cuda"
+        elif torch.backends.mps.is_available():
+            self.device = "mps"
+        else:
+            self.device = "cpu"
+        lseg_model = LSegEncNet("", arch_option=0, block_depth=0, activation="lrelu", crop_size=crop_size)
+        model_state_dict = lseg_model.state_dict()
+        checkpoint_dir = Path(__file__).resolve().parents[1] / "lseg" / "checkpoints"
+        checkpoint_path = checkpoint_dir / "demo_e200.ckpt"
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        if not checkpoint_path.exists():
+            print("Downloading LSeg checkpoint...")
+            # the checkpoint is from official LSeg github repo
+            # https://github.com/isl-org/lang-seg
+            checkpoint_url = "https://drive.google.com/u/0/uc?id=1ayk6NXURI_vIPlym16f_RG3ffxBWHxvb"
+            gdown.download(checkpoint_url, output=str(checkpoint_path))
+
+        pretrained_state_dict = torch.load(checkpoint_path, map_location=self.device)
+        pretrained_state_dict = {k.lstrip("net."): v for k, v in pretrained_state_dict["state_dict"].items()}
+        model_state_dict.update(pretrained_state_dict)
+        lseg_model.load_state_dict(pretrained_state_dict)
+
+        lseg_model.eval()
+        lseg_model = lseg_model.to(self.device)
+
+        norm_mean = [0.5, 0.5, 0.5]
+        norm_std = [0.5, 0.5, 0.5]
+        lseg_transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+            ]
+        )
+        self.clip_feat_dim = lseg_model.out_c
+        return lseg_model, lseg_transform, crop_size, base_size, norm_mean, norm_std
+
+def get_lseg_feat(
+    model: LSegEncNet,
+    image: np.array,
+    labels,
+    transform,
+    device,
+    crop_size=480,
+    base_size=520,
+    norm_mean=[0.5, 0.5, 0.5],
+    norm_std=[0.5, 0.5, 0.5],
+    vis=False,
+):
+    vis_image = image.copy()
+    image = transform(image).unsqueeze(0).to(device)
+    img = image[0].permute(1, 2, 0)
+    img = img * 0.5 + 0.5
+
+    batch, _, h, w = image.size()
+    stride_rate = 2.0 / 3.0
+    stride = int(crop_size * stride_rate)
+
+    # long_size = int(math.ceil(base_size * scale))
+    long_size = base_size
+    if h > w:
+        height = long_size
+        width = int(1.0 * w * long_size / h + 0.5)
+        short_size = width
+    else:
+        width = long_size
+        height = int(1.0 * h * long_size / w + 0.5)
+        short_size = height
+
+    cur_img = resize_image(image, height, width, **{"mode": "bilinear", "align_corners": True})
+
+    if long_size <= crop_size:
+        pad_img = pad_image(cur_img, norm_mean, norm_std, crop_size)
+        print(pad_img.shape)
+        with torch.no_grad():
+            # outputs = model(pad_img)
+            outputs, logits = model(pad_img, labels)
+        outputs = crop_image(outputs, 0, height, 0, width)
+    else:
+        if short_size < crop_size:
+            # pad if needed
+            pad_img = pad_image(cur_img, norm_mean, norm_std, crop_size)
+        else:
+            pad_img = cur_img
+        _, _, ph, pw = pad_img.shape  # .size()
+        assert ph >= height and pw >= width
+        h_grids = int(math.ceil(1.0 * (ph - crop_size) / stride)) + 1
+        w_grids = int(math.ceil(1.0 * (pw - crop_size) / stride)) + 1
+        with torch.cuda.device_of(image):
+            with torch.no_grad():
+                outputs = image.new().resize_(batch, model.out_c, ph, pw).zero_().to(device)
+                logits_outputs = image.new().resize_(batch, len(labels), ph, pw).zero_().to(device)
+            count_norm = image.new().resize_(batch, 1, ph, pw).zero_().to(device)
+        # grid evaluation
+        for idh in range(h_grids):
+            for idw in range(w_grids):
+                h0 = idh * stride
+                w0 = idw * stride
+                h1 = min(h0 + crop_size, ph)
+                w1 = min(w0 + crop_size, pw)
+                crop_img = crop_image(pad_img, h0, h1, w0, w1)
+                # pad if needed
+                pad_crop_img = pad_image(crop_img, norm_mean, norm_std, crop_size)
+                with torch.no_grad():
+                    # output = model(pad_crop_img)
+                    output, logits = model(pad_crop_img, labels)
+                cropped = crop_image(output, 0, h1 - h0, 0, w1 - w0)
+                cropped_logits = crop_image(logits, 0, h1 - h0, 0, w1 - w0)
+                outputs[:, :, h0:h1, w0:w1] += cropped
+                logits_outputs[:, :, h0:h1, w0:w1] += cropped_logits
+                count_norm[:, :, h0:h1, w0:w1] += 1
+        assert (count_norm == 0).sum() == 0
+        outputs = outputs / count_norm
+        logits_outputs = logits_outputs / count_norm
+        outputs = outputs[:, :, :height, :width]
+        logits_outputs = logits_outputs[:, :, :height, :width]
+    # outputs = resize_image(outputs, h, w, **{'mode': 'bilinear', 'align_corners': True})
+    # outputs = resize_image(outputs, image.shape[0], image.shape[1], **{'mode': 'bilinear', 'align_corners': True})
+    outputs = outputs.cpu()
+    outputs = outputs.numpy()  # B, D, H, W
+    predicts = [torch.max(logit, 0)[1].cpu().numpy() for logit in logits_outputs]
+    pred = predicts[0]
+    if vis:
+        new_palette = get_new_pallete(len(labels))
+        mask, patches = get_new_mask_pallete(pred, new_palette, out_label_flag=True, labels=labels)
+        seg = mask.convert("RGBA")
+        cv2.imshow("image", vis_image[:, :, [2, 1, 0]])
+        cv2.waitKey()
+        fig = plt.figure()
+        plt.imshow(seg)
+        plt.legend(handles=patches, loc="upper left", bbox_to_anchor=(1.0, 1), prop={"size": 20})
+        plt.axis("off")
+
+        plt.tight_layout()
+        plt.show()
+
+    return outputs
+
+def get_lseg_score(
+    clip_model,
+    landmarks: list,
+    lseg_map: np.array,
+    clip_feat_dim: int,
+    use_multiple_templates: bool = False,
+    avg_mode: int = 0,
+    add_other=True,
+):
+    """
+    Inputs:
+        landmarks: a list of strings that describe the landmarks
+        lseg_map: a numpy array with shape (h, w, clip_dim) or (N, clip_dim)
+        avg_mode: this is for multiple template. 0 for averaging features, 1 for averaging scores
+    Return:
+        scores_list: (h, w, class_num) storing the score for each location for each class
+    """
+    landmarks_other = landmarks
+    if add_other and landmarks_other[-1] != "other":
+        landmarks_other = landmarks + ["other"]
+
+    if use_multiple_templates:
+        mul_tmp = multiple_templates.copy()
+        multi_temp_landmarks_other = [x.format(lm) for lm in landmarks_other for x in mul_tmp]
+        text_feats = get_text_feats(multi_temp_landmarks_other, clip_model, clip_feat_dim)
+
+        # average the features
+        if avg_mode == 0:
+            text_feats = text_feats.reshape((-1, len(mul_tmp), text_feats.shape[-1]))
+            text_feats = np.mean(text_feats, axis=1)
+
+        map_feats = lseg_map.reshape((-1, lseg_map.shape[-1]))
+
+        scores_list = map_feats @ text_feats.T
+
+        # average the features
+        if avg_mode == 1:
+            scores_list = scores_list.reshape((-1, len(landmarks_other), len(mul_tmp)))
+            scores_list = np.mean(scores_list, axis=2)
+    else:
+        text_feats = get_text_feats(landmarks_other, clip_model, clip_feat_dim)
+
+        map_feats = lseg_map.reshape((-1, lseg_map.shape[-1]))
+
+        scores_list = map_feats @ text_feats.T
+
+    return scores_list
+
 def get_img_feats(img, preprocess, clip_model):
     """
     Get the image features from the CLIP model
