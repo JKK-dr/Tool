40a688296acdf9657410dd30c05ba132c7c97675
Polaris <59070815+JKK-dr@users.noreply.github.com>
2024-10-08 20:57:55 +0800

    Create ply2bin.py

---
+++ ply2bin.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from plyfile import PlyData
+import trimesh
+
+# obj2ply
+def to_ply(input_path, output_path, original_type):
+    mesh = trimesh.load(input_path, file_type=original_type)  # read file
+    mesh.export(output_path, file_type='ply')  # convert to ply
+
+# ply2bin
+def convert_ply(input_path, output_path):
+    plydata = PlyData.read(input_path)  # read file
+    data = plydata.elements[0].data  # read data
+    data_pd = pd.DataFrame(data)  # convert to DataFrame
+    data_np = np.zeros(data_pd.shape, dtype=np.float)  # initialize array to store data
+    property_names = data[0].dtype.names  # read names of properties
+    for i, name in enumerate(
+            property_names):  # read data by property
+        data_np[:, i] = data_pd[name]
+    data_np.astype(np.float32).tofile(output_path)
