a9d7c0a14da43ce023aa77052dbdd9b87407dbbc
Polaris <59070815+JKK-dr@users.noreply.github.com>
2024-10-08 21:59:12 +0800

    Create image.py

---
+++ image.py
@@ -0,0 +1,43 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+# 对图像应用仿射变换
+def get_grid(pose, grid_size, device):
+    """
+    Input:
+        `pose` FloatTensor(bs, 3)
+        `grid_size` 4-tuple (bs, _, grid_h, grid_w)
+        `device` torch.device (cpu or gpu)
+    Output:
+        `rot_grid` FloatTensor(bs, grid_h, grid_w, 2)
+        `trans_grid` FloatTensor(bs, grid_h, grid_w, 2)
+
+    """
+    pose = pose.float()
+    x = pose[:, 0]
+    y = pose[:, 1]
+    t = pose[:, 2]
+
+    bs = x.size(0)
+    t = t * np.pi / 180.
+    cos_t = t.cos()
+    sin_t = t.sin()
+
+    theta11 = torch.stack([cos_t, -sin_t,
+                           torch.zeros(cos_t.shape).float().to(device)], 1)
+    theta12 = torch.stack([sin_t, cos_t,
+                           torch.zeros(cos_t.shape).float().to(device)], 1)
+    theta1 = torch.stack([theta11, theta12], 1)
+
+    theta21 = torch.stack([torch.ones(x.shape).to(device),
+                           -torch.zeros(x.shape).to(device), x], 1)
+    theta22 = torch.stack([torch.zeros(x.shape).to(device),
+                           torch.ones(x.shape).to(device), y], 1)
+    theta2 = torch.stack([theta21, theta22], 1)
+
+    rot_grid = F.affine_grid(theta1, torch.Size(grid_size))
+    trans_grid = F.affine_grid(theta2, torch.Size(grid_size))
+
+    return rot_grid, trans_grid
